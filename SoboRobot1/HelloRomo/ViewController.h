9b24cbb708fcea8b7ea4e1291de4fe366570d920
Laura <laura@Lauras-MacBook-Air.local>
2016-01-26 00:50:19 +0000

    Commit working

--- SoboRobot1/HelloRomo/ViewController.h
+++ SoboRobot1/HelloRomo/ViewController.h
@@ -7,6 +7,8 @@
 #import <RMCore/RMCore.h>
 #import <RMCharacter/RMCharacter.h>
 
+#define INITIAL_HEAD_ANGLE 130.0
+
 @interface ViewController : UIViewController <RMCoreDelegate>
 
 @property (nonatomic, strong) RMCoreRobotRomo3 *Romo3;
--- SoboRobot1/HelloRomo/ViewController.m
+++ SoboRobot1/HelloRomo/ViewController.m
@@ -20,6 +20,8 @@
     [RMCore setDelegate:self];
     
     [self addGestureRecognizers];
+    
+    
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -38,7 +40,7 @@
         // Change Romo's LED to be solid at 80% power
         [self.Romo3.LEDs setSolidWithBrightness:0.8];
         
-        // When we plug Romo in, he get's excited!
+        // When you plug Romo in, he gets excited
         self.Romo.expression = RMCharacterExpressionExcited;
     }
 }
@@ -48,7 +50,7 @@
     if (robot == self.Romo3) {
         self.Romo3 = nil;
         
-        // When we plug Romo in, he get's excited!
+        // When you unplug Romo in, he gets sad
         self.Romo.expression = RMCharacterExpressionSad;
     }
 }
@@ -70,8 +72,16 @@
     swipeUp.direction = UISwipeGestureRecognizerDirectionUp;
     [self.view addGestureRecognizer:swipeUp];
     
+    UISwipeGestureRecognizer *swipeDown = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipedDown:)];
+    swipeDown.direction = UISwipeGestureRecognizerDirectionDown;
+    [self.view addGestureRecognizer:swipeDown];
+    
     UITapGestureRecognizer *tapReceived = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tappedScreen:)];
     [self.view addGestureRecognizer:tapReceived];
+    
+    UITapGestureRecognizer *doubleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(doubleTap:)];
+    doubleTap.numberOfTapsRequired = 2; //Adds double table
+    [self.view addGestureRecognizer:doubleTap];
 }
 
 
@@ -79,6 +89,9 @@
 {
     // When the user swipes left, Romo will turn in a circle to his left
     [self.Romo3 driveWithRadius:-1.0 speed:1.0];
+    sleep(3);
+    [self.Romo3 stopDriving];
+
 }
 
 - (void)swipedRight:(UIGestureRecognizer *)sender
@@ -87,16 +100,26 @@
     [self.Romo3 driveWithRadius:1.0 speed:1.0];
 }
 
-// Swipe up to change Romo's emotion to some random emotion
+// Swipe up
 - (void)swipedUp:(UIGestureRecognizer *)sender
 {
-    int numberOfEmotions = 7;
+    // Tilt up by ten degrees
+    float tiltByAngleInDegrees = 10.0;
+     
+    [self.Romo3 tiltByAngle:tiltByAngleInDegrees
+                  completion:^(BOOL success) {
+                  }];
+}
+
+// Swipe down
+- (void)swipedDown:(UIGestureRecognizer *)sender
+{
+    // Tilt up by ten degrees
+    float tiltByAngleInDegrees = -10.0;
     
-    // Choose a random emotion from 1 to numberOfEmotions
-    // That's different from the current emotion
-    RMCharacterEmotion randomEmotion = 1 + (arc4random() % numberOfEmotions);
-    
-    self.Romo.emotion = randomEmotion;
+    [self.Romo3 tiltByAngle:tiltByAngleInDegrees
+                 completion:^(BOOL success) {
+                 }];
 }
 
 // Simply tap the screen to stop Romo
@@ -104,5 +127,24 @@
 {
     [self.Romo3 stopDriving];
 }
+
+- (void)doubleTap:(UIGestureRecognizer *)sender
+{
+    if (sender.state == UIGestureRecognizerStateRecognized)
+    {
+        // Constants for the number of expression & emotion enum values
+        int numberOfExpressions = 19;
+        int numberOfEmotions = 7;
+        
+        // Choose a random expression from 1 to numberOfExpressions
+        RMCharacterExpression randomExpression = 1 + (arc4random() % numberOfExpressions);
+        
+        // Choose a random expression from 1 to numberOfEmotions
+        RMCharacterEmotion randomEmotion = 1 + (arc4random() % numberOfEmotions);
+        
+        [self.Romo setExpression:randomExpression withEmotion:randomEmotion];
+    }
+}
+
 
 @end
